6c5916d83511596f51cc5a219162dbc4de6a29be
“Michael <michaelrodiles@gmail.com>
2022-01-31 08:33:19 -0500

    to do icons

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import TodoForm from "./TodoForm";
+import Todo from "./Todo";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
@@ -18,6 +19,7 @@ function TodoList() {
     <div>
       <h1>Whats the plan for today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} />
     </div>
   );
 }
